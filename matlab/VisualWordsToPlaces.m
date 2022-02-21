3d0957a0a52d830b982b8c8a7791bbec2607d390
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2022-02-21 16:59:31 +0200

    Update VisualWordsToPlaces.m

--- matlab/VisualWordsToPlaces.m
+++ matlab/VisualWordsToPlaces.m
@@ -10,7 +10,7 @@
 % redistribute it and/or modify it under the terms of the MIT License as 
 % published by the corresponding authors 
 %  
-% Bag-of-Tracked-Words pipeline is distributed in the hope that it will be 
+% Assigning Visual Words to Places pipeline is distributed in the hope that it will be 
 % useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
 % of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
